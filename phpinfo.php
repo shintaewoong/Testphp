2bad635ab27a0804c24adc54adec32eafd2ded80
shintaewoong <150774931+shintaewoong@users.noreply.github.com>
2023-11-17 17:44:32 +0900

    Add files via upload

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,17 @@
 <?php   
-    echo 1;
+    echo "시간당급여";
+    echo "- 주간 근무 : 9,500원";
+    echo "- 야간 근무 : 주간 시급 * 1.5";
+
+    $Priced = 9500;
+    $WorkD = "주간";
+    $WorkT = 8;
+
+    if($WorkD == "주간") {
+        $pay = $WorkT*$Priced;
+    } else {
+        $pay = $WorkT*$Priced * 1.5;
+    }
+
+    echo $pay;
     ?>
